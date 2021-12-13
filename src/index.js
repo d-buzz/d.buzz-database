b964a93a39787144eb1da3d427056a9418719ca2
Aashir Shaikh <hi@aashir.me>
2021-12-13 23:46:17 +0500

    add cors domains

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const api = require('./api')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+  
+app.use(cors(
+  { origin: ['http://localhost:3000', 'http://d.buzz', 'http://next.d.buzz'] }
+))
 
-app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true, limit: 10000000}))
 
