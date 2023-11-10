9038abdd674442bfb3562e5ba9990ee012daa51b
Kenneth Kempis <kennethroykempis@gmail.com>
2023-11-10 18:49:34 +0800

    added changes for hivesql

--- src/api/routes/getRouter.js
+++ src/api/routes/getRouter.js
@@ -27,4 +27,46 @@ getRouter.get('/', function (req, res) {
 	}
 })
 
+getRouter.get('/getLeaderboardEngagement', async function (req, res) {
+
+	const hivesql_config = {
+		user: 'Hive-k3mpz',
+		password: 'kZSPP448F4AkHTL4HbN4',
+		server: 'vip.hivesql.io', 
+		database: 'DBHive',
+		options: {
+		  enableArithAbort: true,
+		  encrypt: true,
+		  trustServerCertificate: true,
+		},
+	}
+	const mssql = require("mssql")
+    const {sort, date_type} = req.query
+    let date_filter = ''
+    if(date_type === 'all_time'){
+      date_filter = ''
+    }else if(date_type === 'weekly'){
+      date_filter = " AND DATEPART(WK, c.created) = DATEPART(WK, GETDATE()) AND DATEPART(YEAR, c.created) = DATEPART(YEAR, GETDATE())"
+    }else{
+      date_filter =  " AND CONVERT(DATE, c.created) = CONVERT(DATE, GETDATE())"
+    }
+    let sort_by = ' GROUP BY c.author ORDER BY score '
+    if(sort === 'asc'){
+      sort_by+='ASC'
+    }else{
+      sort_by+='DESC'
+    }
+    try {
+      const pool = await mssql.connect(hivesql_config)
+      const query = "SELECT TOP 10 c.author, (SUM(c.children)+SUM(c.net_votes)) as score FROM Comments c WHERE ISJSON(c.json_metadata) > 0 AND JSON_VALUE(c.json_metadata, '$.app') IS NOT NULL AND JSON_VALUE(c.json_metadata , '$.app') = 'dBuzz/v3.0.0'"+date_filter+sort_by
+      const result = await pool.request().query(query)
+	  res.send(result.recordset)
+    } catch (err) {
+      console.error('Error executing query:', err)
+      res.send(err)
+    } finally {
+      mssql.close()
+    }
+})
+
 module.exports = getRouter
